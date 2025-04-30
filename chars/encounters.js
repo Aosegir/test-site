436497c432457b72316637a21befe893be939558
Aosegir <128326345+Aosegir@users.noreply.github.com>
2025-04-30 07:11:51 -0700

    improved createEncounter function

--- chars/encounters.js
+++ chars/encounters.js
@@ -2,78 +2,70 @@ let monsterTable = [
     {
         name: "Bone Rabble",
         xp: 25,
+        area: "Citadel",
     },
     {
         name: "Bone Soldier",
         xp: 100,
+        area: "Citadel",
     },
     {
         name: "Bone Defender",
         xp: 100,
+        area: "Citadel",
     },
     {
         name: "Bone Arbalist",
         xp: 100,
+        area: "Citadel",
+    },
+    //BREAK
+    {
+        name: "Mimic",
+        xp: 450,
+        area: "Study",
     },
 ];
 
-let geniusTable = {
-    0: ["Mimic"],
-    1: ["Smothering Rug"],
-    2: ["Animated Armor"],
-};
-
-let kalisTable = {
-    0: ["Death Knight"],
-    1: ["Arbalest Squad"],
-    2: ["Kill Team"],
-};
-
-let ennaTable = {
-    0: ["Fungal Scratcher"],
-    1: ["Fungal Artillery", "Fungal Scratcher"],
-    2: ["Large Ectoplasm", "Ectoplasm"],
-    3: ["Unclean Giant"],
-};
-
-let vorasTable = {
-    0: ["Wyrmlings"],
-    1: ["Kobolds"],
-    2: ["Hell Hounds"],
-};
-
-let juriTable = {
-    0: ["Bulette"],
-    1: ["Ropers"],
-    2: ["Lava Mephits"],
-};
-
-let animaTable = {
-    0: ["Bone Rabble"],
-    1: ["Brigand Cutthroat", "Brigand Cutthroat", "Brigand Cuttrhoat"],
-    2: ["Brigand Bloodletter"],
-    3: ["The Thing From The Stars"],
-};
-
+/*
+    WHAT: determineDifficulty function returns a random XP threshhold based on number of characters and their level
+    WHY: this function exists to create variety in the amount of monsters in each encounter
+*/
 function determineDifficulty(characters, level) {
     let threshholds = [];
     threshholds.push(characters * level * 25);
     threshholds.push(characters * level * 50);
     threshholds.push(characters * level * 75);
     threshholds.push(characters * level * 100);
-    return selectFromTable(threshholds);
+    return threshholds[Math.floor(Math.random() * threshholds.length)];
 };
 
-function selectFromTable(table) {
-    return table[Math.floor(Math.random() * Object.keys(table).length)];
+function filterMonsterList(table, zone) {
+    return table.filter((monster) => monster.area === zone);
 };
 
-function createEncounter() {
+/*
+    WHAT: selectMonster function returns a random item from an array of object, filtered by a specific zone
+    WHY: this function exists to randomize any pull from the monsterTable list from the correct zone
+*/
+function selectMonster(table, zone) {
+    do {
+        let areaList = filterMonsterList(table, zone);
+        monster = areaList[Math.floor(Math.random() * table.length)];
+    } while(!monster);
+    return monster;
+};
+
+/*
+    WHAT: createEncounter function creates a random encounter for any given area
+    WHY: i mean this is like the whole point of random encounters
+*/
+function createEncounter(zone) {
     let runningEncounter = [];
     let runningXP = 0;
     let encounterXP = determineDifficulty(4, 1);
     while(runningXP <= encounterXP) {
-        let currentMonster = selectFromTable(monsterTable);
+        let currentMonster = selectMonster(monsterTable, zone);
         runningEncounter.push(currentMonster.name);
         runningXP += currentMonster.xp;
     };
@@ -91,26 +83,24 @@ let genius = document.getElementById("genius");
 
 if(kalis) {
     kalis.addEventListener('click', () => {
-        randomEncounter.innerText = createEncounter();
+        randomEncounter.innerText = createEncounter("Citadel");
     });    
 } else if(genius) {
     genius.addEventListener('click', () => {
-        randomEncounter.innerText = selectFromTable(geniusTable);
+        randomEncounter.innerText = createEncounter("Study");
     });
 } else if(enna) {
     enna.addEventListener('click', () => {
-        randomEncounter.innerText = selectFromTable(ennaTable);
     });
 } else if(voras) {
     voras.addEventListener('click', () => {
-        randomEncounter.innerText = selectFromTable(vorasTable);
+
     });
 } else if(juri) {
     juri.addEventListener('click', () => {
-        randomEncounter.innerText = selectFromTable(juriTable);
+
     });
 } else if(anima) {
     anima.addEventListener('click', () => {
-        randomEncounter.innerText = selectFromTable(animaTable);
     });
 };
